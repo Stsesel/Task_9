1aa4676bd96a4c6c19fea071f46ec4d7bdb1b41b
asvartgroupspb@gmail.com <asvartgroupspb@gmail.com>
2022-01-01 15:16:32 +0300

    Добавьте файлы проекта.

---
+++ Task_9/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstValue, secondValue;
+                string action;
+
+                Console.WriteLine("Это мой микрокалькулятор");
+
+                try
+                {
+                    Console.WriteLine("Введите первое число");
+                    firstValue = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число");
+                    secondValue = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Необходимо ввести число");
+                    Console.ReadLine();
+                    continue;
+                }
+
+
+
+                Console.WriteLine("Выберите операцию: '+', '-', '*', '/'");
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+
+                    case "+":
+                        Console.WriteLine(firstValue + secondValue);
+                        break;
+
+                    case "-":
+                        Console.WriteLine(firstValue - secondValue);
+                        break;
+
+                    case "*":
+                        Console.WriteLine(firstValue * secondValue);
+                        break;
+
+                    case "/":
+                        if (secondValue == 0)
+                        {
+                            Console.WriteLine(0);
+                        }
+                        else
+                        {
+                            Console.WriteLine(firstValue / secondValue);
+                        }
+                        
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка! Неизвестное действие");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
